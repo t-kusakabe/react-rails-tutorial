b024992bc6f8018a985d450bf3840fb978be1e3c
t-kusakabe <ttmac0704@gmail.com>
2016-12-27 13:33:05 +0900

    setting application

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,17 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ReactRailsTutorial
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
+
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
   end
 end

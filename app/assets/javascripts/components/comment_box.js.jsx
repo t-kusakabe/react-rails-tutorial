5afe6fc4d06c7c2fbe87178be5393af388dd91ce
t-kusakabe <ttmac0704@gmail.com>
2016-12-28 05:36:52 +0900

    add comment view

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -1,9 +1,28 @@
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(result) {
+        this.setState({data: result.data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={ this.props.data } />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -28,11 +47,24 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = React.findDOMNode(thid.refs.author).value.trim();
+    var text = React.findDOMNode(this.refs.text).value.trim();
+    if (!text || !author) {
+      return;
+    }
+    React.findDOMNode(this.refs.author).value = '';
+    React.findDOMNode(this.refs.text).value = '';
+    return;
+  },
   render: function() {
     return (
-      <div className='commentForm'>
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className='commentForm'>
+        <input type='text' placeholder='Your name' />
+        <input type='text' placeholder='Say something...' />
+        <input type='submit' value='Post' />
+      </form>
     );
   }
 });
